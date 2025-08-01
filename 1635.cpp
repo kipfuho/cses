09b4012977c8b7b2874c820db44fbb01922cffe4
kipfuho <ctcakip@gmail.com>
2025-08-01 15:34:55 +0700

    feat: code

--- 1635.cpp
+++ 1635.cpp
@@ -20,7 +20,6 @@ int main() {_
             }
         }
     }
-
     
     cout << dp[x];
     return 0;
--- 1636.cpp
+++ 1636.cpp
@@ -13,14 +13,11 @@ int main() {_
 
     vector<int> dp(x + 1, 0);
     dp[0] = 1;
-    for (int i = 1; i <= x; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i - coins[j] >= 0 && dp[i - coins[j]] > 0) {
-                dp[i] = (dp[i] + dp[i - coins[j]]) % 1000000007;
-            }
+    for (int i = 0; i < n; i++) {
+        for (int j = coins[i]; j <= x; j++) {
+            dp[j] = (dp[j] + dp[j - coins[i]]) % 1000000007;
         }
     }
-
     
     cout << dp[x];
     return 0;
